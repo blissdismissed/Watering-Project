4bb318e0779ef443ca32c8ea49a3253b8aa71558
Arthur <ajautz@gmail.com>
2019-09-19 11:58:47 -0400

    working code for spi interface with capacitive sensor.

--- analogspitest.py
+++ analogspitest.py
@@ -11,6 +11,7 @@ spi = spidev.SpiDev()
 spi.open(0,0)
 
 def readChannel(channel):
+    spi.max_speed_hz = 1350000
     val = spi.xfer2([1,(8+channel)<<4,0])
     data = ((val[1]&3) << 8) + val[2]
     return data
@@ -19,9 +20,9 @@ if __name__ == "__main__":
     try:
         while True:
             val = readChannel(0) #reading from channel 0
-            print(val)
+            print("Moisture:", val)
             if (val != 0):
-                print(val)
+                print("Moisture:", val)
             sleep(delay)
             
     except KeyboardInterrupt:
